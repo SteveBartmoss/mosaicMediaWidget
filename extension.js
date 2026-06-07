4553226229106b58c80ca4f706c539c444ece4ca
Tony <za19012397@zapopan.tecmm.edu.mx>
2026-06-06 18:30:46 -0600

    feat: Se agregan mejoras para poder abrir un menu contextual

--- extension.js
+++ extension.js
@@ -2,64 +2,270 @@ import St from 'gi://St';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 export default class ExampleExtension extends Extension {
     enable() {
 
-        this.currentIndex = 0;
         this.images = [];
+        this.currentIndex = 0;
+        this.paused = false;
 
         this.loadImages();
 
-        if(this.images.length === 0){
+        if (this.images.length === 0) {
             console.error('MOSAI: No se encontraron imágenes');
             return;
         }
 
-        console.log('MOSAI: enable() ejecutado');
+        this.shuffleImages();
 
-        this.widget = new St.Widget({
-            style: this.buildImageStyle(this.images[0])
-        });
+        this.createWidget();
+        this.createMenu();
 
-        console.log('MOSAI: widget creado');
+        this.startSlideshow();
 
-        Main.layoutManager.addChrome(this.widget);
+        this.startFolderWatcher();
 
         console.log('MOSAI: widget agregado');
 
-        this.widget.set_position(100, 100);
+    }
 
-        this.widget.set_position(100, 100);
+    createWidget() {
 
-        this.timeoutId = GLib.timeout_add_seconds(
-            GLib.PRIORITY_DEFAULT,
-            5,
+        this.widget = new St.Widget({
+            style: this.buildImageStyle(
+                this.images[this.currentIndex]
+            )
+        });
+
+        Main.layoutManager.addChrome(
+            this.widget
+        );
+
+        this.widget.set_position(
+            100,
+            100
+        );
+    }
+
+    createMenu() {
+
+        this._indicator =
+            new PanelMenu.Button(
+                0,
+                'Mosai'
+            );
+
+        const icon = new St.Icon({
+            icon_name: 'image-x-generic-symbolic',
+            style_class: 'system-status-icon',
+        });
+
+        this._indicator.add_child(icon);
+
+        Main.panel.addToStatusArea(
+            this.uuid,
+            this._indicator
+        );
+
+        const nextItem =
+            new PopupMenu.PopupMenuItem(
+                'Next Image'
+            );
+
+        nextItem.connect(
+            'activate',
+            () => this.nextImage()
+        );
+
+        this._indicator.menu.addMenuItem(
+            nextItem
+        );
+
+        const prevItem =
+            new PopupMenu.PopupMenuItem(
+                'Previous Image'
+            );
+
+        prevItem.connect(
+            'activate',
+            () => this.previousImage()
+        );
+
+        this._indicator.menu.addMenuItem(
+            prevItem
+        );
+
+        const pauseItem =
+            new PopupMenu.PopupMenuItem(
+                'Pause'
+            );
+
+        pauseItem.connect(
+            'activate',
             () => {
-
-                this.nextImage();
-
-                return GLib.SOURCE_CONTINUE;
+                this.paused = true;
             }
         );
 
-        console.log('MOSAI: posición establecida');
+        this._indicator.menu.addMenuItem(
+            pauseItem
+        );
 
-        // Create a panel button
-        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
+        const resumeItem =
+            new PopupMenu.PopupMenuItem(
+                'Resume'
+            );
 
-        // Add an icon
-        const icon = new St.Icon({
-            icon_name: 'face-laugh-symbolic',
-            style_class: 'system-status-icon',
-        });
-        this._indicator.add_child(icon);
+        resumeItem.connect(
+            'activate',
+            () => {
+                this.paused = false;
+            }
+        );
 
-        // Add the indicator to the panel
-        Main.panel.addToStatusArea(this.uuid, this._indicator);
+        this._indicator.menu.addMenuItem(
+            resumeItem
+        );
+
+        const reloadItem =
+            new PopupMenu.PopupMenuItem(
+                'Reload Folder'
+            );
+
+        reloadItem.connect(
+            'activate',
+            () => {
+                this.reloadImages();
+            }
+        );
+
+        this._indicator.menu.addMenuItem(
+            reloadItem
+        );
+    }
+
+    shuffleImages() {
+
+        for (
+            let i = this.images.length - 1;
+            i > 0;
+            i--
+        ) {
+
+            const j =
+                Math.floor(
+                    Math.random() * (i + 1)
+                );
+
+            [
+                this.images[i],
+                this.images[j]
+            ] = [
+                    this.images[j],
+                    this.images[i]
+                ];
+        }
+    }
+
+    reloadImages() {
+
+        this.images = [];
+
+        this.loadImages();
+
+        if (this.images.length === 0) {
+
+            console.warn(
+                'MOSAI: carpeta vacía'
+            );
+
+            return;
+        }
+
+        this.shuffleImages();
+
+        this.currentIndex = 0;
+
+        this.updateImage();
+    }
+
+    startSlideshow() {
+
+        this.timeoutId =
+            GLib.timeout_add_seconds(
+                GLib.PRIORITY_DEFAULT,
+                5,
+                () => {
+
+                    if (!this.paused) {
+                        this.nextImage();
+                    }
+
+                    return GLib.SOURCE_CONTINUE;
+                }
+            );
+    }
+
+    previousImage() {
+
+        this.currentIndex--;
+
+        if (
+            this.currentIndex < 0
+        ) {
+            this.currentIndex =
+                this.images.length - 1;
+        }
+
+        this.updateImage();
+    }
+
+    updateImage() {
+
+        this.widget.set_style(
+            this.buildImageStyle(
+                this.images[
+                this.currentIndex
+                ]
+            )
+        );
+    }
+
+    disable() {
+
+        console.log(
+            'MOSAI: disable() ejecutado'
+        );
+
+        this.images = [];
+
+        if (
+            this.timeoutId
+        ) {
+
+            GLib.Source.remove(
+                this.timeoutId
+            );
+
+            this.timeoutId = null;
+        }
+
+        this.monitor?.cancel();
+
+        this.monitor = null;
+
+        this.widget?.destroy();
+
+        this.widget = null;
+
+        this._indicator?.destroy();
+
+        this._indicator = null;
     }
 
     loadImages() {
@@ -100,13 +306,21 @@ export default class ExampleExtension extends Extension {
 
     }
 
-    buildImageStyle(imagePath) {
+    buildImageStyle(path) {
 
         return `
-            background-image: url("${imagePath}");
-            background-size: contain;
-            background-repeat: no-repeat;
-            background-position: center;
+            background-image:
+                url("${path}");
+
+            background-size:
+                contain;
+
+            background-repeat:
+                no-repeat;
+
+            background-position:
+                center;
+
             width: 300px;
             height: 300px;
         `;
@@ -120,31 +334,52 @@ export default class ExampleExtension extends Extension {
             this.currentIndex = 0;
         }
 
-        const image = this.images[this.currentIndex];
+        console.log(
+            `MOSAI: mostrando ${this.images[this.currentIndex]
+            }`
+        );
 
-        console.log(`MOSAI: mostrando ${image}`);
+        this.updateImage();
+    }
 
-        this.widget.set_style(
-            this.buildImageStyle(image)
+    startFolderWatcher() {
+
+        const folder =
+            Gio.File.new_for_path(
+                `${this.path}/img`
+            );
+
+        this.monitor =
+            folder.monitor_directory(
+                Gio.FileMonitorFlags.NONE,
+                null
+            );
+
+        this.reloadTimeout = null;
+
+        this.monitor.connect(
+            'changed',
+            () => {
+
+                if (this.reloadTimeout)
+                    return;
+
+                this.reloadTimeout =
+                    GLib.timeout_add_seconds(
+                        GLib.PRIORITY_DEFAULT,
+                        1,
+                        () => {
+
+                            this.reloadTimeout =
+                                null;
+
+                            this.reloadImages();
+
+                            return GLib.SOURCE_REMOVE;
+                        }
+                    );
+            }
         );
     }
 
-
-    disable() {
-
-        console.log('MOSAI: disable() ejecutado');
-
-        if (this.timeoutId) {
-            GLib.Source.remove(this.timeoutId);
-            this.timeoutId = null;
-        }
-
-        this.widget?.destroy();
-        this.widget = null;
-
-        this.images = [];
-
-        this._indicator?.destroy();
-        this._indicator = null;
-    }
 }
